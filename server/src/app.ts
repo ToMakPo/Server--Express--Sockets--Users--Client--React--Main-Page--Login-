81ac330cc5ac65845b84066899417bf753ec1ccb
Makai Post <postmakai@gmail.com>
2025-03-13 13:00:48 -0700

    add dotenv for environment variable management; update server configuration to use dynamic port

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
@@ -12,7 +16,11 @@ app.use(express.json());
 
 // Basic route
 app.get('/', (req, res) => {
-    res.render('home', { title: 'HomeX' });
+    res.render('home');
+});
+
+app.get('/login', (req, res) => {
+	res.render('login');
 });
 
 // Start the server
